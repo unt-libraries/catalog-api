481c3c6f51366cb2c8ec9e9395f03e54ba28a05b
Jason Thomale <jason.thomale@unt.edu>
2016-04-12 14:31:18 -0500

    Add check for DJANGO_SETTINGS_MODULE to manage.py
    This fixes an issue running manage.py commands via an init.d script,
    such as running Celery in a production environment, where the init.d
    script sets the DJANGO_SETTINGS_MODULE environment variable and the
    relative path to the settings.json file doesn't work. Now it checks
    for and uses the DJANGO_SETTINGS_MODULE already set if it exists
    before trying to access the settings.json file.

--- django/sierra/manage.py
+++ django/sierra/manage.py
@@ -5,10 +5,11 @@ import sys
 import ujson
 
 if __name__ == '__main__':
-    with open('sierra/settings/settings.json') as f:
-        local_settings = ujson.loads(f.read())
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 
-                          local_settings.get('SETTINGS_MODULE'))
+    if os.environ.get('DJANGO_SETTINGS_MODULE', None) is None:
+        with open('sierra/settings/settings.json') as f:
+            local_settings = ujson.loads(f.read())
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 
+                              local_settings.get('SETTINGS_MODULE'))
 
     from django.core.management import execute_from_command_line
 
