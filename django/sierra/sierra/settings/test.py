ae852a20d79c675c4a21b900c1395f298666e14c
jthomale <jason.thomale@unt.edu>
2017-06-14 09:10:49 -0500

    Change settings to use .env file for secrets
    As a first step toward Dockerization, this changeset modifies how
    secrets and local settings (DB names, hosts, passwords) are stored.
    Docker-compose allows you to pull environment variables from a .env
    file, and so it seems to make sense to use the same .env file format to
    communicate secrets both to Django settings and to docker-compose. This
    way we don't have to have separate settings files defining the exact
    same settings.

--- django/sierra/sierra/settings/test.py
+++ django/sierra/sierra/settings/test.py
@@ -1,9 +1,11 @@
 from .base import *
 
-required_test_db_setting_suffixes = ('USER', 'PASSWORD', 'HOST')
-for suffix in required_test_db_setting_suffixes:
-    setting = 'TEST_SIERRA_DB_{}'.format(suffix)
-    if local_settings.get(setting, None) is None:
+# Check required TEST settings
+required = ['TEST_SIERRA_DB_USER', 'TEST_SIERRA_DB_PASSWORD',
+            'TEST_DEFAULT_DB_USER', 'TEST_DEFAULT_DB_PASSWORD']
+
+for setting in required:
+    if get_env_variable(setting) is None:
         raise_setting_error(setting)
 
 DEBUG = True
@@ -15,19 +17,51 @@ INSTALLED_APPS += (
 )
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'django_sierra_test',
-    },
     'sierra': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'iii_test',
-        'USER': local_settings['TEST_SIERRA_DB_USER'],
-        'PASSWORD': local_settings['TEST_SIERRA_DB_PASSWORD'],
-        'HOST': local_settings['TEST_SIERRA_DB_HOST'],
-        'PORT': local_settings.get('TEST_SIERRA_DB_PORT', None),
-        'TEST': {
-            'NAME': 'iii_test'
-        }
+        'NAME': get_env_variable('TEST_SIERRA_DB_NAME', 'sierra_test'),
+        'USER': get_env_variable('TEST_SIERRA_DB_USER'),
+        'PASSWORD': get_env_variable('TEST_SIERRA_DB_PASSWORD'),
+        'HOST': get_env_variable('TEST_SIERRA_DB_HOST', '127.0.0.1'),
+        'PORT': get_env_variable('TEST_SIERRA_DB_PORT', '5432')
+    },
+    'default': {
+        'ENGINE': get_env_variable('TEST_DEFAULT_DB_ENGINE',
+                                   'django.db.backends.mysql'),
+        'NAME': get_env_variable('TEST_DEFAULT_DB_NAME', 'capi_test'),
+        'USER': get_env_variable('TEST_DEFAULT_DB_USER'),
+        'PASSWORD': get_env_variable('TEST_DEFAULT_DB_PASSWORD'),
+        'HOST': get_env_variable('TEST_DEFAULT_DB_HOST', '127.0.0.1'),
+        'PORT': get_env_variable('TEST_DEFAULT_DB_PORT', '3306')
     }
 }
+
+# CELERY settings
+REDIS_CELERY_PORT = get_env_variable('TEST_REDIS_CELERY_PORT', '6279')
+REDIS_CELERY_HOST = get_env_variable('TEST_REDIS_CELERY_HOST', 'localhost')
+redis_celery_url = 'redis://{}:{}/0'.format(REDIS_CELERY_HOST,
+                                            REDIS_CELERY_PORT)
+BROKER_URL = redis_celery_url
+CELERY_RESULT_BACKEND = redis_celery_url
+
+# Other Redis settings
+REDIS_CONNECTION = {
+    'host': get_env_variable('TEST_REDIS_APPDATA_HOST', 'localhost'),
+    'port': get_env_variable('TEST_REDIS_APPDATA_PORT', '6280'),
+    'db': get_env_variable('TEST_REDIS_APPDATA_DATABASE', 0)
+}
+
+# Solr settings
+SOLR_PORT = get_env_variable('TEST_SOLR_PORT', '8883')
+SOLR_HOST = get_env_variable('TEST_SOLR_HOST', 'localhost')
+solr_haystack_url = get_env_variable('SOLR_HAYSTACK_URL', 
+                    'http://{}:{}/solr/haystack'.format(SOLR_HOST, SOLR_PORT))
+solr_bibdata_url = get_env_variable('SOLR_BIBDATA_URL', 
+                    'http://{}:{}/solr/bibdata'.format(SOLR_HOST, SOLR_PORT))
+solr_marc_url = get_env_variable('SOLR_MARC_URL', 
+                    'http://{}:{}/solr/marc'.format(SOLR_HOST, SOLR_PORT))
+
+HAYSTACK_CONNECTIONS['default']['URL'] = solr_haystack_url
+HAYSTACK_CONNECTIONS['haystack']['URL'] = solr_haystack_url
+HAYSTACK_CONNECTIONS['bibdata']['URL'] = solr_bibdata_url
+HAYSTACK_CONNECTIONS['marc']['URL'] = solr_marc_url
