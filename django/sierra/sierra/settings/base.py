f8dc00f052d2f3ee9eaa0a27ad2bb1e20d64af27
jthomale <jason.thomale@unt.edu>
2017-06-20 11:25:47 -0500

    Fix issue with parsing of boolean values from .env file

--- django/sierra/sierra/settings/base.py
+++ django/sierra/sierra/settings/base.py
@@ -10,7 +10,8 @@ from django.core.exceptions import ImproperlyConfigured
 
 def get_env_variable(var_name, default=None):
     """Get the environment variable or return default value"""
-    return os.environ.get(var_name, default)
+    val = os.environ.get(var_name, default)
+    return True if val == 'true' else False if val == 'false' else val
 
 
 def raise_setting_error(setting):
