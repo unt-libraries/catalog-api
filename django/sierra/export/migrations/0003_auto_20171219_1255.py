03aab80abf24a3ac68c3d4593526d54a6343560f
jthomale <jason.thomale@unt.edu>
2017-12-19 09:59:52 -0600

    Create and configure new blacklight Django app
    This adds a new Django app to the project to contain all blacklight-related
    development, specifically exporters. It creates the skeleton for a
    blacklight-staging exporter process, which we'll set up more fully
    later.

---
+++ django/sierra/export/migrations/0003_auto_20171219_1255.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from utils.load_data import load_data
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('export', '0002_auto_20170620_1001'),
+    ]
+
+    operations = [
+         migrations.RunPython(
+            load_data('export/migrations/data/blacklight_staging.json',
+                      'default')),
+    ]
