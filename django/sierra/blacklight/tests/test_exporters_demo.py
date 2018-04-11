131decb5b9ca0ba1a6bfb4cb4143c9f4eaf0dbd0
jthomale <jason.thomale@unt.edu>
2018-04-11 10:23:01 -0500

    Set up exporter for Blacklight demo instance

---
+++ django/sierra/blacklight/tests/test_exporters_demo.py
@@ -0,0 +1,30 @@
+"""
+Tests the `blacklight.exporters_demo` classes.
+"""
+
+import pytest
+
+
+# FIXTURES AND TEST DATA
+# Fixtures used in the below tests can be found in
+# django/sierra/conftest.py:
+#    sierra_records_by_recnum_range
+# django/sierra/blacklight/tests/conftest.py
+#    export_to_solr
+
+pytestmark = pytest.mark.django_db
+
+# TESTS
+
+def test_bibs_to_blacklight_demo(sierra_records_by_recnum_range,
+                                 export_to_solr):
+    """
+    This is a very basic sanity test to make sure that the
+    BibsToBlacklightDemo exporter loads and deletes from the correct
+    Solr core.
+    """
+    record_set = sierra_records_by_recnum_range('b4371446')
+    results = export_to_solr('bl-demo', record_set, 'BibsToBlacklightDemo')
+    assert len(results['pre']) == 0
+    assert len(results['load']) > 0
+    assert len(results['del']) == 0
