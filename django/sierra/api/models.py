fd26bf82be923b0b9a7fde4764291b806b2f208e
jthomale <jason.thomale@unt.edu>
2019-01-25 16:40:58 -0600

    Update APIUser model and add custom manager

--- django/sierra/api/models.py
+++ django/sierra/api/models.py
@@ -11,6 +11,8 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.contrib.auth.models import User
 
+from api.managers import APIUserManager
+
 
 def get_permission_defaults_from_apps():
     """
@@ -36,36 +38,111 @@ class APIUser(models.Model):
     secret = models.CharField(max_length=128)
     permissions = models.TextField(default='{}')
     permission_defaults = get_permission_defaults_from_apps()
+    objects = APIUserManager()
 
     def __init__(self, *args, **kwargs):
+        """
+        When an APIUser object is initialized, the `secret` and
+        `permissions` fields may be set via a `secret_text` and
+        `permissions_dict` kwarg, respectively.
+
+        `secret_text` is the user-readable text of the secret, which is
+        encoded to produce the `secret` field value.
+
+        `permissions_dict` is a Python dict w/permissions to override
+        the defaults.
+        """
+        permissions_dict = kwargs.pop('permissions_dict', {})
+        secret_text = kwargs.pop('secret_text', None)
         super(APIUser, self).__init__(*args, **kwargs)
-        my_perms = ujson.decode(self.permissions)
-        all_perms = type(self).permission_defaults.copy()
-        all_perms.update(my_perms)
-        self.permissions_dict = all_perms
+        self.update_permissions(permissions_dict)
+        if not self.secret and secret_text is not None:
+            self.secret = self.encode_secret(secret_text)
 
     def save(self, *args, **kwargs):
-        self.permissions = ujson.encode(self.permissions_dict)
-        super(APIUser, self).save(*args, **kwargs)        
+        """
+        An APIUser MUST have a `secret` and a `user` relation before
+        the object is saved; otherwise, an APIUserException is raised.
+        """
+        if not self.secret:
+            msg = 'APIUser obj cannot be saved without a `secret`.'
+            raise APIUserException(msg)
+        try:
+            self.user
+        except User.DoesNotExist:
+            msg = 'APIUser obj cannot be saved without a related user.'
+            raise APIUserException(msg)
+        super(APIUser, self).save(*args, **kwargs)
 
-    def set_permissions(self, permissions, value):
+    def update_and_save(secret_text=None, permissions_dict=None, email=None,
+                        password=None, first_name=None, last_name=None):
+        """
+        Update AND SAVE an existing APIUser with any or all new values.
+        For any of `email`, `password`, `first_name`, and `last_name`,
+        the related User object is updated with the appropriate
+        value(s).
+        """
+        user_kwargs = {'password': password, 'email': email,
+                       'first_name': first_name, 'last_name': last_name}
+        if any([v is not None for v in user_kwargs.values()]):
+            for field, value in user_kwargs.items():
+                if value is not None:
+                    setattr(self.user, field, value)
+            self.user.save()
+        if secret_text:
+            self.secret = self.encode_secret(secret_text)
+        if permissions_dict:
+            self.update_permissions(permissions_dict)
+        self.save()
+        return self
+
+    def update_permissions(self, permissions_dict):
+        """
+        Update certain permissions' values via a `permissions_dict`.
+
+        The passed `permissions_dict` is a dictionary with key-value
+        pairs that set particular permissions (keys) to specific bool
+        values.
+
+        Names for the permissions that are set MUST exist in
+        cls.permission_defaults. An APIUserException is raised if an
+        unexpected permission is encountered.
+
+        Returns a dictionary of all user permissions after the update.
+        """
+        permissions = type(self).permission_defaults.copy()
+        permissions.update(ujson.decode(self.permissions))
+        for pname, pvalue in permissions_dict.items():
+            if pname in permissions:
+                permissions[pname] = pvalue
+            else:
+                msg = 'Permission `{}` is not valid.'.format(pname)
+                raise APIUserException(msg)
+        self.permissions = ujson.encode(permissions)
+        return permissions
+
+    def set_permissions_to_value(self, permissions, value):
         """
         Set certain permissions to the given `value` for this APIUser.
         
         `permissions` is a list of permissions to set to the supplied
         boolean value.
-        """
-        for perm in permissions:
-            if perm in self.permissions_dict:
-                self.permissions_dict[perm] = value
 
-    def set_all_permissions(self, value):
+        Returns a dictionary of all user permissions after the update.
+        """
+        return self.update_permissions({pname: value for pname in permissions})
+
+    def set_all_permissions_to_value(self, value):
         """
         Set ALL permissions for this APIUser to the given value.
-        """
-        self.set_permissions(self.permissions_dict.keys(), value)
 
-    def make_secret(self, secret, hash_type='sha256'):
+        Returns a dictionary of all user permissions after the update.
+        """
+        permissions = self.permission_defaults.keys()
+        return self.set_permissions_to_value(permissions, value)
+
+    @staticmethod
+    def encode_secret(secret, hash_type='sha256'):
         """
         Make and return a `secret` string for this APIUser.
 
--- django/sierra/api/tests/test_models.py
+++ django/sierra/api/tests/test_models.py
@@ -7,11 +7,16 @@ resource (including authentication) is tested via `test_api.py`.
 
 import pytest
 
+import ujson
+
+from api.models import APIUserException
+
 
 # FIXTURES AND TEST DATA
 # ---------------------------------------------------------------------
 # External fixtures used below can be found in
 # django/sierra/conftest.py:
+#   model_init_instance
 #
 # django/sierra/api/tests/conftest.py:
 #   apiuser_with_custom_defaults
@@ -21,39 +26,73 @@ import pytest
 # TESTS
 # ---------------------------------------------------------------------
 
-
-def test_apiuser_setpermissions(apiuser_with_custom_defaults):
+@pytest.mark.django_db
+def test_apiuser_save_errors_with_no_secret(apiuser_with_custom_defaults,
+                                            model_init_instance):
     """
-    The `set_permissions` method should set the provided boolean
+    Attempting to save an APIUser that has no secret should raise
+    an APIUserException.
+    """
+    test_cls = apiuser_with_custom_defaults()
+    u = model_init_instance(test_cls)
+    assert u.secret == ''
+    with pytest.raises(APIUserException):
+        u.save()
+
+
+@pytest.mark.django_db
+def test_apiuser_save_errors_with_no_user(apiuser_with_custom_defaults,
+                                          model_init_instance):
+    """
+    Attempting to save an APIUser that has no related user should raise
+    an APIUserException.
+    """
+    test_cls = apiuser_with_custom_defaults()
+    u = model_init_instance(test_cls, secret_text='secret')
+    with pytest.raises(APIUserException):
+        u.save()
+
+
+@pytest.mark.django_db
+def test_apiuser_setpermissionstovalue(apiuser_with_custom_defaults,
+                                       model_init_instance):
+    """
+    The `set_permissions_to_value` method should set the provided boolean
     value on the given list of permissions.
     """
     custom_defaults = {'first': False, 'second': False, 'third': False}
     test_cls = apiuser_with_custom_defaults(custom_defaults)
-    u = test_cls()
-    u.set_permissions(['first', 'third'], True)
-    assert u.permissions_dict == {'first': True, 'second': False,
-                                  'third': True}
+    u = model_init_instance(test_cls)
+    u.set_permissions_to_value(['first', 'third'], True)
+    assert ujson.decode(u.permissions) == {'first': True, 'second': False,
+                                           'third': True}
 
-def test_apiuser_setpermissions_missing_perm(apiuser_with_custom_defaults):
+
+@pytest.mark.django_db
+def test_apiuser_setpermissionstovalue_error(apiuser_with_custom_defaults,
+                                             model_init_instance):
     """
-    The `set_permissions` method should ignore any permissions in the
+    The `set_permissions_to_value` method should raise an
+    APIUserException if it encounters any permissions in the
     permissions list that aren't registered as default permissions.
     """
     custom_defaults = {'first': False, 'second': False, 'third': False}
     test_cls = apiuser_with_custom_defaults(custom_defaults)
-    u = test_cls()
-    u.set_permissions(['first', 'third', 'fourth'], True)
-    assert u.permissions_dict == {'first': True, 'second': False,
-                                  'third': True}
+    u = model_init_instance(test_cls)
+    with pytest.raises(APIUserException):
+        u.set_permissions_to_value(['first', 'third', 'fourth'], True)
 
-def test_apiuser_setallpermissions(apiuser_with_custom_defaults):
+
+@pytest.mark.django_db
+def test_apiuser_setallpermissionstovalue(apiuser_with_custom_defaults,
+                                          model_init_instance):
     """
-    The `set_all_permissions` method should set the provided boolean
+    The `set_all_permissions_to_value` method should set the provided boolean
     value on ALL available permissions.
     """
     custom_defaults = {'first': False, 'second': False, 'third': False}
     test_cls = apiuser_with_custom_defaults(custom_defaults)
-    u = test_cls()
-    u.set_all_permissions(True)
-    assert u.permissions_dict == {'first': True, 'second': True,
-                                  'third': True}
+    u = model_init_instance(test_cls)
+    u.set_all_permissions_to_value(True)
+    assert ujson.decode(u.permissions) == {'first': True, 'second': True,
+                                           'third': True}
