5b07f23060fa7819bc0fcf103adca71e8e4cd5ff
jthomale <jason.thomale@unt.edu>
2019-02-27 10:16:46 -0600

    Fix a few code formatting issues

--- django/sierra/shelflist/exporters.py
+++ django/sierra/shelflist/exporters.py
@@ -1,11 +1,12 @@
-'''
+"""
 Exporters for the Shelflist app are defined here. See export.exporter
 for the base Exporter class and export.basic_exporters for some
 implementations. Here we pretty much just need to define the
 final_callback method for item exporters so that any locations
 represented in the items have their shelflist row manifest document in
 Solr updated automatically whenever items are loaded.
-'''
+"""
+
 from __future__ import unicode_literals
 import logging
 import json
@@ -24,11 +25,11 @@ from . import search_indexes as indexes
 logger = logging.getLogger('sierra.custom')
 
 def index_shelflist_rows(exp):
-    '''
+    """
     This is what does the work of determining the locations that need
     to be updated (based on what locations appear in the exporter
     object's record set) and stuff.
-    '''
+    """
     exp.log('Info', 'Creating shelflist item manifests.')
     solr = pysolr.Solr(settings.HAYSTACK_CONNECTIONS[exp.hs_conn]['URL'])
     records = exp.get_records()
@@ -40,8 +41,8 @@ def index_shelflist_rows(exp):
                 'q': '*:*',
                 'fq': ['type:Item', 'location_code:{}'.format(location)],
                 'fl': 'id',
-                'sort': 'call_number_type asc, call_number_sort asc, volume_sort '
-                        'asc, copy_number asc',
+                'sort': 'call_number_type asc, call_number_sort asc, '
+                        'volume_sort asc, copy_number asc',
             }
             hits = solr.search(rows=0, **params).hits
             results = solr.search(rows=hits, **params)
@@ -53,6 +54,7 @@ def index_shelflist_rows(exp):
 class ItemsToSolr(exporters.ItemsToSolr):
     max_rec_chunk = 500
     index_class = indexes.ShelflistItemIndex
+    
     def final_callback(self, vals={}, status='success'):
         super(ItemsToSolr, self).final_callback(vals, status)
         index_shelflist_rows(self)
