f044ce33110f623c67828f1e9cec198d82b1c4c4
jthomale <jason.thomale@unt.edu>
2019-05-16 12:57:07 -0500

    Add IIILanguage rel to Bibs exp prefetch_related
    I noticed that the BibsToSolr exporter is about 20% slower in the
    update-dependencies-jt branch than the one in master, and after
    doing a bit of profiling I traced it back to the MARC conversion
    routines in sierra2marc. From there I traced it to the implementation of
    the base.models.ModelWithAttachedName class, particularly with the
    base.models.MaterialPropertyName model. The IIILanguage model is used to
    filter the appropriate name in the appropriate language. I added this
    table to the `prefetch_related` attr in the applicable exporter, which
    made a small improvement.

--- django/sierra/export/basic_exporters.py
+++ django/sierra/export/basic_exporters.py
@@ -363,7 +363,9 @@ class BibsDownloadMarc(Exporter):
         'bibrecorditemrecordlink_set__item_record__record_metadata'
             '__record_type',
         'bibrecordproperty_set',
+        'bibrecordproperty_set__material__materialpropertyname_set',
         'bibrecordproperty_set__material__materialpropertyname_set'
+            '__iii_language',
     ]
     select_related = ['record_metadata', 'record_metadata__record_type']
 
