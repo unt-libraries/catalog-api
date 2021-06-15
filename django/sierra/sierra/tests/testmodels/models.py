eac193285d2617d55311a958ff016fabfe5649a7
Jason Thomale <jason.thomale@unt.edu>
2021-06-15 14:58:23 -0500

    Add more detailed information to module docstring
    This is to help document the intent behind these test models for future
    reference.

--- django/sierra/sierra/tests/testmodels/models.py
+++ django/sierra/sierra/tests/testmodels/models.py
@@ -1,5 +1,25 @@
 """
-Contains models used only for testing purposes.
+Contains models used only for testing relationtrees and makefixtures.
+
+These test models function as test fixtures or input data for the tests
+that use them. E.g., `relationtrees` is designed to traverse relations
+on Django models and build tree-like structures to represent them.
+These models emulate the various common relationships you might have in
+a live database, and tests ensure that the relevant code can traverse
+them correctly.
+
+Summary of model structure:
+
+RN => SRN => SRN => End
+          => End
+   <= ThN => M2M => End
+   -> One
+   => End
+
+Where:
+=> indicates a Forward Many to One relationship
+<= indicates a Reverse Many to One relationship
+-> indicates a Forward One to One relationship
 """
 from __future__ import absolute_import
 from django.db import models
@@ -24,6 +44,7 @@ class ThroughNode(models.Model):
     name = models.CharField(max_length=255)
     ref = models.ForeignKey('ReferenceNode', on_delete=models.CASCADE)
     m2m = models.ForeignKey(ManyToManyNode, on_delete=models.CASCADE)
+
 
 class OneToOneNode(models.Model):
     name = models.CharField(max_length=255)
