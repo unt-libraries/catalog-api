c13b8a78ec446716067dc40592d68b78108efc01
Jason Thomale <jason.thomale@unt.edu>
2021-09-10 15:09:41 -0500

    Fix celery integration test woes
    This should: update `export/tests/test_task_integration.py` to the
    latest version and fix the `CELERY_WORKER_SEND_TASK_EVENTS` option.

--- django/sierra/export/tests/test_tasks_integration.py
+++ django/sierra/export/tests/test_tasks_integration.py
@@ -13,7 +13,6 @@ from export import tasks as export_tasks
 
 # pytestmark = pytest.mark.django_db
 
-
 @pytest.fixture(scope='function')
 def export_and_monitor(global_new_export_instance, django_db_blocker):
     """
@@ -37,6 +36,9 @@ def export_and_monitor(global_new_export_instance, django_db_blocker):
     tasks will run before they finish. Using too low a value will stop
     them prematurely. You will have to experiment.)
     """
+    class StopMonitor(Exception):
+        pass
+
     class CeleryExportMonitor(object):
         def __init__(self, celery_app):
             self.celery_app = celery_app
@@ -46,9 +48,10 @@ def export_and_monitor(global_new_export_instance, django_db_blocker):
             self.exp = None
             if exp_type and exp_filter:
                 self.exp = self.make_exporter(exp_type, exp_filter, options)
-            self.export_started = False
             self.celery_app.events.Receiver.should_stop = False
             self.state = self.celery_app.events.State()
+            self.log = ['Reset. Waiting to start.']
+            self.status = 'waiting'
 
         def make_exporter(self, exp_type, exp_filter, options):
             options = options or {}
@@ -68,32 +71,54 @@ def export_and_monitor(global_new_export_instance, django_db_blocker):
             task = self.state.tasks.get(event['uuid'])
             is_final_task = task.name.endswith('do_final_cleanup')
             task_done = event['type'] not in ('task-received', 'task-started')
-            return is_final_task and task_done
+            return is_final_task and task_done 
 
         def stop(self):
-            self.exp.instance.refresh_from_db()
             self.celery_app.events.Receiver.should_stop = True
 
         def on_heartbeat(self, event):
-            if not self.export_started:
-                self.export_started = True
+            self.log.append('Heartbeat: clock {}'.format(event['clock']))
+            if self.status == 'capturing':
+                self.status = 'export started'
+                self.log.append('Starting exporter.')
                 self.start_export()
+            elif self.status == 'stopping':
+                self.log.append('Stop signal did not work: stopping on HB')
+                raise StopMonitor()
 
         def on_task_event(self, event):
             prefix, etype = event['type'].split('-')
             if prefix == 'task':
                 self.state.event(event)
+                task = self.state.tasks.get(event['uuid'])
+                self.log.append(
+                    '{} {}: clock {}'.format(
+                        event['type'], task.name, event['clock']
+                    )
+                )
             if self.should_stop(event):
+                self.log.append('Sending signal to stop.')
+                self.status = 'stopping'
                 self.stop()
 
         def start(self, exp_type, exp_filter, options, limit=None):
             self.reset(exp_type, exp_filter, options)
             with self.celery_app.connection() as conn:
+                self.log.append('Connected to worker: {}'.format(conn))
+                self.status = 'capturing'
                 recv = self.celery_app.events.Receiver(conn, handlers={
                     'worker-heartbeat': self.on_heartbeat,
                     '*': self.on_task_event,
                 })
-                recv.capture(limit=limit, timeout=None, wakeup=True)
+                try:
+                    recv.capture(limit=limit, timeout=None, wakeup=True)
+                except StopMonitor:
+                    pass
+
+            if self.status != 'stopping':
+                self.log.append('Exited before stop signal (hit `limit`?)')
+            self.log.append('Done capturing.')
+            self.exp.instance.refresh_from_db()
             self.celery_app.events.Receiver.should_stop = False
 
     def _export_and_monitor(exp_type, exp_filter, options, limit=None):
@@ -108,31 +133,47 @@ def export_and_monitor(global_new_export_instance, django_db_blocker):
 
 @pytest.mark.parametrize('exp_type, exp_filter, options, num_expected_batches, '
                          'num_expected_chunks, exp_solr_results, wait_limit', [
-    ('LocationsToSolr', 'full_export', {}, 1, 1, {'haystack': 131}, None),
-    ('ItypesToSolr', 'full_export', {}, 1, 1, {'haystack': 100}, None),
-    ('ItemStatusesToSolr', 'full_export', {}, 1, 1, {'haystack': 22}, None),
-    ('AllMetadataToSolr', 'full_export', {}, 1, 3, {'haystack': 253}, None),
-    ('ItemsToSolr', 'full_export', {}, 1, 2, {'haystack': 269}, None),
-    ('EResourcesToSolr', 'full_export', {}, 1, 2, {'haystack': 1}, None),
+    ('LocationsToSolr', 'full_export', {}, 1, 1, {'haystack': 131}, 30),
+    ('ItypesToSolr', 'full_export', {}, 1, 1, {'haystack': 100}, 30),
+    ('ItemStatusesToSolr', 'full_export', {}, 1, 1, {'haystack': 22}, 30),
+    ('AllMetadataToSolr', 'full_export', {}, 1, 3, {'haystack': 253}, 30),
+    ('ItemsToSolr', 'full_export', {}, 1, 2, {'haystack': 269}, 30),
+    ('EResourcesToSolr', 'full_export', {}, 1, 2, {'haystack': 1}, 30),
     ('ItemsBibsToSolr', 'full_export', {}, 1, 2,
-     {'haystack': 269, 'bibdata': 0}, None),
+     {'haystack': 269, 'bibdata': 0}, 30),
     ('BibsAndAttachedToSolr', 'full_export', {}, 1, 4,
-     {'haystack': 269, 'bibdata': 0}, None),
-    ('BibsToDiscover', 'full_export', {}, 1, 2, {'discover-02': 261}, None),
+     {'haystack': 269, 'bibdata': 0}, 30),
+    ('BibsToDiscover', 'full_export', {}, 1, 2, {'discover-02': 261}, 30),
     ('BibsToDiscoverAndAttachedToSolr', 'full_export', {}, 1, 2,
-     {'haystack': 269, 'bibdata': 0, 'discover-02': 261}, None),
+     {'haystack': 269, 'bibdata': 0, 'discover-02': 261}, 30),
 ])
 def test_export_tasks(exp_type, exp_filter, options, num_expected_chunks,
                       num_expected_batches, exp_solr_results, wait_limit,
                       solr_conns, export_and_monitor):
+    """
+    Exporters that run via Celery tasks should work correctly. This is
+    a complete integration test for all of the available exporters,
+    running the `full_export` process. The following should be true for
+    the test to pass:
+    - A task should have run for each expected chunk, each expected
+      batch, plus two additional tasks (delegate_batch and
+      do_final_cleanup).
+    - All tasks should succeed.
+    - The last task should be do_final_cleanup.
+    - The exporter instance should report a status code of 'success.'
+    - The correct Solr core(s) should have the correct number of
+      records. All other Solr cores should be empty.
+    - The JobPlan (used to corral the tasks for a given export) should
+      have an empty registry and no unprocessed chunks, indicating it
+      has successfully been cleared out.
+    """
     monitor = export_and_monitor(exp_type, exp_filter, options, wait_limit)
     tasks = monitor.state.tasks.values()
     plan = export_tasks.JobPlan(monitor.exp)
 
-    # Print the captured tasks to stdout if the test fails
-    for t in tasks:
-        print(' '.join([t.name, t.state, str(t.received), str(t.started),
-                        str(t.succeeded or t.failed)]))
+    # Print the monitor log if test fails:
+    for entry in monitor.log:
+        print('*MONITOR* {}'.format(entry))
 
     assert len(tasks) == num_expected_chunks + num_expected_batches + 2
     assert all([t.succeeded for t in tasks])
@@ -143,3 +184,5 @@ def test_export_tasks(exp_type, exp_filter, options, num_expected_chunks,
     for name, conn in solr_conns.items():
         if name != 'default':
             assert conn.search('*:*').hits == exp_solr_results.get(name, 0)
+
+
--- django/sierra/sierra/settings/test.py
+++ django/sierra/sierra/settings/test.py
@@ -56,7 +56,7 @@ redis_celery_url = 'redis://{}:{}/0'.format(REDIS_CELERY_HOST,
                                             REDIS_CELERY_PORT)
 CELERY_BROKER_URL = redis_celery_url
 CELERY_RESULT_BACKEND = redis_celery_url
-CELERY_SEND_EVENTS = True
+CELERY_WORKER_SEND_TASK_EVENTS = True
 
 # Other Redis settings
 REDIS_CONNECTION = {
