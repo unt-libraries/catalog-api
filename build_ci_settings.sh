7f92e0e46dd9b5efef716b0707c49b8655e8a7ee
Jason Thomale <jason.thomale@unt.edu>
2023-12-15 12:23:50 -0600

    Fix issues with GitHub actions tests
    Trying to fix three issues here with the GitHub actions test run:
    1. 'DJANGO_SETTINGS_MODULE' variable not set. I've corrected this in
       the `build_ci_settings.sh` file.
    2. The 'manage-test' docker-compose service appeared to get built in the
       middle of the init-dockerdata process, when it should have been fully
       built as part of the previous step. I changed the order of services
       in that previous build step to put 'manage-test' first. I'm not sure
       if this will have any effect.
    3. Had one failing test, in exporter task integration tests -- the
       number of expected chunks that tasks got broken into didn't match
       reality. In my own testing environment I have a couple of extra
       settings that set the max batch size, which would probably have a
       direct effect on how many chunks get produced. I copied this setting
       into the `build_ci_settings.sh` file.

--- build_ci_settings.sh
+++ build_ci_settings.sh
@@ -2,7 +2,7 @@
 
 path_to_settings=django/sierra/sierra/settings/.env
 test_settings="SECRET_KEY=\"Some secret key\"
-SETTINGS_MODULE=sierra.settings.test
+DJANGO_SETTINGS_MODULE=sierra.settings.test
 ALLOWED_HOSTS=localhost
 SIERRA_DB_USER=none
 SIERRA_DB_PASSWORD=none
@@ -20,6 +20,8 @@ TIME_ZONE=America/Chicago
 EXPORTER_EMAIL_ON_ERROR=false
 EXPORTER_EMAIL_ON_WARNING=false
 EXPORTER_AUTOMATED_USERNAME=django_admin
+EXPORTER_MAX_RC_CONFIG=\"ItemsToSolr:500,ItemsBibsToSolr:500,BibsToSolr:500,BibsAndAttachedToSolr:100\"
+EXPORTER_MAX_DC_CONFIG=\"ItemsToSolr:1000,ItemsBibsToSolr:100,BibsToSolr:100,BibsAndAttachedToSolr:100\"
 "
 
 echo "$test_settings" > $path_to_settings
